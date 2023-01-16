22c01f3780aaa3c5a4551c95f26ca20b29cbe7d3
Aleksey <lexx29@yandex.ru>
2023-01-16 19:11:51 +0300

    homework25

---
+++ homework27/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Вариант решения №1 
+
+int SumDigit(int N)
+{
+int lenght = N.ToString().Length;
+int sum = 0;
+int sum2 = 0;
+for ( int i = lenght; i >0; i--)
+{
+int sum1 = N / Convert.ToInt32(Math.Pow(10, i-1));
+sum+= sum1 - sum2*10;
+sum2 = sum1;
+}
+return sum;
+}
+ int GetNumberFromConsole(string message)
+ {
+    Console.WriteLine( message );
+    return int.Parse(Console.ReadLine());
+ }
+    int N = GetNumberFromConsole ("A =");
+     Console.Write("Сумма цифр числа =");
+     Console.WriteLine(SumDigit(N));
+
+// Вариант решения №2
+
+// int SumDigit(int N)
+// {
+// while (N>1)
+// {
+// int sum+ = N % 10 ;
+// }
+// return sum;
+// }
+//  int GetNumberFromConsole(string message)
+//  {
+//     Console.WriteLine( message );
+//     return int.Parse(Console.ReadLine());
+//  }
+//     int N = GetNumberFromConsole ("A =");
+//      Console.Write("Сумма цифр числа =");
+//      Console.WriteLine(SumDigit(N));
---
+++ homework29/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray (int sizeArray)
+{
+int[] array = new int[sizeArray];
+Random rnd = new Random();
+for (int i =0; i<array.Length; i++)
+array[i] = rnd.Next(-100, 100);
+return array;
+}
+
+Console.WriteLine("введите колличество элементов массива");
+int arraySize = int.Parse(Console.ReadLine());
+int[] array = CreateArray (arraySize);
+Console.WriteLine(string.Join(",", array));
